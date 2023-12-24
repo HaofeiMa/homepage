06114adeb30c94597c56f0bb3efc76c4f93e8095
Haofei Ma <haofei.ma@qq.com>
2023-12-24 19:54:55 +0800

    Change iup categories

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,14 +33,15 @@ function getBingImages(imgUrls) {
 	 * 先使用 GitHub Action 每天获取 Bing 壁纸 URL 并更新 images.json 文件
 	 * 然后读取 images.json 文件中的数据
 	 */
-	var indexName = "bing-image-index";
-	var index = sessionStorage.getItem(indexName);
-	var panel = document.querySelector('#panel');
-	if (isNaN(index) || index == 7) index = 0;
-	else index++;
-	var imgUrl = imgUrls[index];
-	var url = "https://www.cn.bing.com" + imgUrl;
-	panel.style.background = "url('" + url + "') center center no-repeat #666";
+	// var indexName = "bing-image-index";
+	// var index = sessionStorage.getItem(indexName);
+	// var panel = document.querySelector('#panel');
+	// if (isNaN(index) || index == 7) index = 0;
+	// else index++;
+	// var imgUrl = imgUrls[index];
+	// var url = "https://www.cn.bing.com" + imgUrl;
+	// panel.style.background = "url('" + url + "') center center no-repeat #666";
+	panel.style.background = "url('https://api.dujin.org/bing/1920.php') center center no-repeat #666";
 	panel.style.backgroundSize = "cover";
 	sessionStorage.setItem(indexName, index);
 }
@@ -59,7 +60,7 @@ document.addEventListener('DOMContentLoaded', function () {
 			document.getElementById('description').innerHTML = res.hitokoto + "<br/> -「<strong>" + res.from + "</strong>」";
 		}
 	};
-	xhr.open("GET", "https://v1.hitokoto.cn", true);
+	xhr.open("GET", "https://v1.hitokoto.cn/?c=d&c=e&c=i&c=k", true);
 	xhr.send();
 
 	var iUpElements = document.querySelectorAll(".iUp");
